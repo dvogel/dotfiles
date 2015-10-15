42624cb9deb5e3956a817b6ea2bd9ee41d209b4b
Drew Vogel <dvogel@github>
2015-10-14 21:34:15 -0700

    vim: update C support

--- vim/.vim/c-support/codesnippets/main.cc
+++ vim/.vim/c-support/codesnippets/main.cc
@@ -1,6 +1,7 @@
-#include	<fstream>
-#include	<iomanip>   
-#include	<iostream>  
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>   
+#include <iostream>  
 
 using namespace std;
 
--- vim/.vim/c-support/scripts/wrapper.sh
+++ vim/.vim/c-support/scripts/wrapper.sh
@@ -4,33 +4,26 @@
 #         USAGE:  ./wrapper.sh executable [cmd-line-args] 
 #   DESCRIPTION:  Wraps the execution of a programm or script.
 #                 Use with xterm: xterm -e wrapper.sh executable cmd-line-args
-#                 This script is used by several plugins:
-#                  bash-support.vim, c.vim and perl-support.vim
+#                 This script is used by the plugins c.vim 
 #       OPTIONS:  ---
-#  REQUIREMENTS:  which(1) - shows the full path of (shell) commands.
+#  REQUIREMENTS:  ---
 #          BUGS:  ---
 #         NOTES:  ---
-#        AUTHOR:  Dr.-Ing. Fritz Mehner (Mn), mehner@fh-swf.de
+#        AUTHOR:  Dr.-Ing. Fritz Mehner (fgm), mehner.fritz@fh-swf.de
 #       COMPANY:  Fachhochschule Südwestfalen, Iserlohn
-#       VERSION:  1.2
 #       CREATED:  23.11.2004 18:04:01 CET
-#      REVISION:  17.03.2005 - executable quoted
-#      REVISION:  18.02.2006 - look for the full pathname of an executable
 #===============================================================================
 
-command=${@}                             # the complete command line
-executable=${1}                          # name of the executable; may be quoted
-
-fullname=$(which $executable)
-[ $? -eq 0 ] && executable=$fullname
+executable="${1}"                               # name of the executable
 
 if [ ${#} -ge 1 ] && [ -x "$executable" ]
 then
-  shift
-  "$executable" ${@}
-  echo -e "\n\n\"${command}\" returned ${?}"
+  "${@}"
+  returncode=$?
+  [ $returncode -ne 0 ] && printf "'${@}' returned ${returncode}\n"
 else
-  echo -e "\n  !! file \"${executable}\" does not exist or is not executable !!"
+  printf "\n  !! file \"${executable}\" does not exist or is not executable !!\n"
+  returncode=126                                # command invoked cannot execute
 fi
-echo -e "  ... press return key ... "
-read dummy
+read -p "  ... press return key ... " dummy
+exit $returncode
